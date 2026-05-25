b22fa0eb76ab47985f490d5bef3e5917846f650c
khattrishashwat <shashwat.k@luckpay.in>
2026-05-25 10:03:09 +0530

    backend

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config();
-
+require("dotenv").config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 const scheduler = require('./src/utils/scheduler');
